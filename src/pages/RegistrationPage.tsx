38306746b646db47845fcc361615ce92b5849428
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-01-23 11:12:15 +0100

    feat: page for registration

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { RegistrationFormRefs } from "../components/RegistrationFormRefs";
 
 type FormState = {
@@ -7,17 +8,30 @@ type FormState = {
 };
 
 export const RegistrationPage = () => {
+  const [step, setStep] = useState<number>(1); // 1, 2, 3
   const handleSubmit = (data: FormState) => {
     console.log({ data });
+  };
+
+  // let Content: React.ReactNode = RegistrationFormRefs;
+  // <Content />
+
+  const renderFirstStep = () => {
+    // if () {
+
+    // }
+    return (
+      <RegistrationFormRefs
+        data={{ email: "patryk@wp.pl" }}
+        onSubmit={handleSubmit}
+      />
+    );
   };
 
   return (
     <div>
       <h1>Registration page</h1>
-      <RegistrationFormRefs
-        data={{ email: "patryk@wp.pl" }}
-        onSubmit={handleSubmit}
-      />
+      {step === 1 && renderFirstStep()}
     </div>
   );
 };
