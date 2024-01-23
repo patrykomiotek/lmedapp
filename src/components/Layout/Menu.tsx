f5a785e3287e1f5477e3fecca95bfd5f784d69c0
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-01-23 11:45:04 +0100

    feat: nav menu

--- src/components/Layout/Menu.tsx
+++ src/components/Layout/Menu.tsx
@@ -1,17 +1,38 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const Menu = () => {
   return (
     <div className="mb-4 border-b-2 border-stone-900">
       <ul className="flex mt-2">
         <li className="mr-4">
-          <Link to="/">Home</Link>
+          <NavLink
+            to="/"
+            className={({ isActive, isPending }) =>
+              isActive ? "text-red-400" : ""
+            }
+          >
+            Home
+          </NavLink>
         </li>
         <li className="mr-4">
-          <Link to="/registration">Registration</Link>
+          <NavLink
+            to="/registration"
+            className={({ isActive, isPending }) =>
+              isActive ? "text-red-400" : ""
+            }
+          >
+            Registration
+          </NavLink>
         </li>
         <li className="mr-4">
-          <Link to="/contact">Contact</Link>
+          <NavLink
+            to="/contact"
+            className={({ isActive, isPending }) =>
+              isActive ? "text-red-400" : ""
+            }
+          >
+            Contact
+          </NavLink>
         </li>
       </ul>
     </div>
